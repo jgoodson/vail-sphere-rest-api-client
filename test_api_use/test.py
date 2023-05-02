b004c12446ff715ebc1d31d52542520e6b3090d2
Jonathan Goodson <goodsonjr@hpc.nih.gov>
2023-05-02 17:18:49 -0400

    Add comments to example

--- test_api_use/test.py
+++ test_api_use/test.py
@@ -1,22 +1,31 @@
 from vail_sphere_rest_api_client import Client, AuthenticatedClient
 from vail_sphere_rest_api_client.api.default import create_token_credential, create_bucket
-from vail_sphere_rest_api_client.models import policy_document, policy_document_statement, ApiToken, ApiCredentials, ApiBucket, ServerValidationErrorResponse
+from vail_sphere_rest_api_client.models import (
+    ApiToken, ApiCredentials, 
+    ApiBucket, PolicyDocument, PolicyDocumentStatement, 
+    ServerValidationErrorResponse
+    )
 
+# Create the unauthenticated client
 c = Client(
     base_url="https://10.1.128.31:8443",
     raise_on_unexpected_status=True
 )
 
+# Construct the credentials object
 api_creds = ApiCredentials(
     "Administrator",
     password="NotTheRealPassword"
 )
 
+# Synchronous API call to get token
 token = create_token_credential.sync(client=c, json_body=api_creds)
 
+# Check whether token returned properly
 if not isinstance(token, ApiToken):
     raise ConnectionError("Failed to authenticate to API")
 
+# Check whether return actually has a valid token in it
 if isinstance(token.token, str):
     api = AuthenticatedClient(
         "https://10.1.128.31:8443",
@@ -25,20 +34,30 @@ if isinstance(token.token, str):
 else:
     raise ConnectionError("Failed to get authentication token")
 
-
-bucket_policy = policy_document.PolicyDocument(
-    statement=policy_document_statement.PolicyDocumentStatement.from_dict(
-            {"statement": "I don't know what goes in here."}
-        ),
+# Create a bucket with a Bucket Policy
+# Policies are just JSON documents with str key/values and can do all sorts of stuff
+# We create them from a Python dict, presumably with all strings.
+bucket_policy = PolicyDocument(
+    statement=PolicyDocumentStatement.from_dict({
+        "Principal": "*",
+        "Effect": "Deny", # The safest bucket - the kind no one can use
+    }),
     version="0.0.1"
     )
+
+# Create the request object from a Model. Some arguments are base types, some are Models themselves like policy
 bucket_create = create_bucket.ApiBucketCreate(
     "test_bucket",
     compress=True,
     encrypt=False,
     policy=bucket_policy
     )
+
+# Actually create the bucket. 
 bucket = create_bucket.sync(client=api, json_body=bucket_create)
 
+# More return type checking since this API likes to return ServerValidationErrorResponse objects
+# We could check for those explicity if they return useful information and ever actually happen
+# Probably work logging or something
 if not isinstance(bucket, ApiBucket):
     raise 
