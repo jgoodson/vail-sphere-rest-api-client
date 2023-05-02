de33ec1fcb5268fb9a0a88adcde898ff29baaf13
Jonathan Goodson <goodsonjr@hpc.nih.gov>
2023-05-02 17:09:47 -0400

    Slight improvement to example

--- test_api_use/test.py
+++ test_api_use/test.py
@@ -1,27 +1,29 @@
 from vail_sphere_rest_api_client import Client, AuthenticatedClient
-from vail_sphere_rest_api_client.api.default import create_token_credential
-from vail_sphere_rest_api_client.api.default import create_bucket
-from vail_sphere_rest_api_client.models import policy_document, policy_document_statement
+from vail_sphere_rest_api_client.api.default import create_token_credential, create_bucket
+from vail_sphere_rest_api_client.models import policy_document, policy_document_statement, ApiToken, ApiCredentials, ApiBucket, ServerValidationErrorResponse
 
 c = Client(
     base_url="https://10.1.128.31:8443",
     raise_on_unexpected_status=True
 )
 
-api_creds = create_token_credential.ApiCredentials(
+api_creds = ApiCredentials(
     "Administrator",
     password="NotTheRealPassword"
 )
 
 token = create_token_credential.sync(client=c, json_body=api_creds)
 
-if type(token) == create_token_credential.ApiToken and type(token.token) == str:
+if not isinstance(token, ApiToken):
+    raise ConnectionError("Failed to authenticate to API")
+
+if isinstance(token.token, str):
     api = AuthenticatedClient(
         "https://10.1.128.31:8443",
         token=token.token
     )
 else:
-    raise TypeError("Failed to get token")
+    raise ConnectionError("Failed to get authentication token")
 
 
 bucket_policy = policy_document.PolicyDocument(
@@ -36,4 +38,7 @@ bucket_create = create_bucket.ApiBucketCreate(
     encrypt=False,
     policy=bucket_policy
     )
-create_bucket.sync(client=api, json_body=bucket_create)
+bucket = create_bucket.sync(client=api, json_body=bucket_create)
+
+if not isinstance(bucket, ApiBucket):
+    raise 
