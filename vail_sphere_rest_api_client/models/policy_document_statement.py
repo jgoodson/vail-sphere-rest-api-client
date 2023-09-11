8abbaae85cd1194bcaba19164dc1d9e3f6078c7d
Jonathan Goodson <jonathan.goodson@nih.gov>
2023-09-11 08:13:44 -0400

    Changes to allow multiple bucket policy statements

--- vail_sphere_rest_api_client/models/policy_document_statement.py
+++ vail_sphere_rest_api_client/models/policy_document_statement.py
@@ -10,6 +10,7 @@ class PolicyDocumentStatement:
     """ """
 
     additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+    multi_statement: bool = attr.ib(False)
 
     def to_dict(self) -> Dict[str, Any]:
         field_dict: Dict[str, Any] = {}
@@ -18,12 +19,36 @@ class PolicyDocumentStatement:
 
         return field_dict
 
+    def to_object(self) -> Dict[str, Any] | List[Any]:
+        if self.multi_statement:
+            statement_list: List[Any] = []
+            statement_list.extend(self.additional_properties["statements"])
+            return statement_list
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        return field_dict
+
     @classmethod
     def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
         d = src_dict.copy()
         policy_document_statement = cls()
 
         policy_document_statement.additional_properties = d
+        return policy_document_statement
+    
+    @classmethod
+    def from_object(cls: Type[T], src_obj: Dict[str, Any] | List[Any]) -> T:
+        d = src_obj.copy()
+        policy_document_statement = cls()
+
+        if isinstance(d, dict):
+            policy_document_statement.additional_properties = d
+            policy_document_statement.multi_statement = False
+        else:
+            policy_document_statement.additional_properties = {"statements": d}
+            policy_document_statement.multi_statement = True
         return policy_document_statement
 
     @property
