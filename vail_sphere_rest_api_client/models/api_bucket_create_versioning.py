ff12cce1c421efd694816105f01c5a0f58ef9a97
Jonathan Goodson <jonathan.goodson@nih.gov>
2024-02-23 12:39:12 -0500

    Update deps and cleanup
    Add mypy
    
    Update deps
    
    Add pyenv file to ignore
    
    Update dependencies

--- vail_sphere_rest_api_client/models/api_bucket_create_versioning.py
+++ vail_sphere_rest_api_client/models/api_bucket_create_versioning.py
@@ -4,6 +4,7 @@ from enum import Enum
 class ApiBucketCreateVersioning(str, Enum):
     ENABLED = "Enabled"
     SUSPENDED = "Suspended"
+    DISABLED = ""
 
     def __str__(self) -> str:
         return str(self.value)
