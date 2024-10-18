cd72ca947b88986f8c0466c2c63e051f26a7eb6c
Jonathan Goodson <jonathan.goodson@nih.gov>
2024-10-18 09:50:31 -0400

    Add DISBALED to versioning enums

--- vail_sphere_rest_api_client/models/api_bucket_versioning.py
+++ vail_sphere_rest_api_client/models/api_bucket_versioning.py
@@ -4,6 +4,7 @@ from enum import Enum
 class ApiBucketVersioning(str, Enum):
     ENABLED = "Enabled"
     SUSPENDED = "Suspended"
+    DISABLED = ""
 
     def __str__(self) -> str:
         return str(self.value)
